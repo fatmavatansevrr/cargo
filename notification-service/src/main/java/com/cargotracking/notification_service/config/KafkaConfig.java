2e849df64a33ca5a529c86a93372b5d200ade371
fatmavatansevrr <vatanseverfatma01@gmail.com>
2025-06-16 22:27:42 +0300

    fatma

--- notification-service/src/main/java/com/cargotracking/notification_service/config/KafkaConfig.java
+++ notification-service/src/main/java/com/cargotracking/notification_service/config/KafkaConfig.java
@@ -25,7 +25,7 @@ import java.util.Map;
 @Slf4j
 public class KafkaConfig {
     
-    @Value("${spring.kafka.bootstrap-servers:localhost:9092}")
+    @Value("${spring.kafka.bootstrap-servers:kafka:29092}")
     private String bootstrapServers;
     
     @Value("${spring.kafka.consumer.group-id:notification-service}")
